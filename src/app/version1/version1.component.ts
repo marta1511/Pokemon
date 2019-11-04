e849d91c6d1412dcdb858ee42db9470df2370e3b
marta1511 <50339660+marta1511@users.noreply.github.com>
2019-11-04 12:59:59 +0100

    paginator, search

--- src/app/version1/version1.component.ts
+++ src/app/version1/version1.component.ts
@@ -2,7 +2,9 @@ import {Component, OnInit, ViewChild} from '@angular/core';
 import {PokemonService} from '../pokemon.service';
 import {Pokemon} from '../pokemons';
 import {MatPaginator, MatTableDataSource, PageEvent} from '@angular/material';
-
+import {MatButtonModule} from '@angular/material/button';
+import set = Reflect.set;
+import {disableDebugTools} from '@angular/platform-browser';
 
 
 
@@ -16,11 +18,13 @@ export class Version1Component implements OnInit {
   pageSizeOptions: number[] = [5, 10, 20, 50, 100];
   totalCount: number = 0;
   currentPage: number = 0;
-  currenPageSize: number = 10;
+  currentPageSize: number = 5;
+  range: number[] = [0, 20]
   pokemons: Pokemon[] = [];
   pokemonDetails: Pokemon[] = [];
   dataSource: MatTableDataSource<Pokemon>;
-  // pageEvent: PageEvent;
+  name = '';
+  type = '';
 
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
@@ -31,15 +35,10 @@ export class Version1Component implements OnInit {
 
   ngOnInit() {
     this.pokemons = [];
-
-
-    this.service.getPokemonList(this.currentPage * this.currenPageSize, this.currenPageSize).subscribe(resp => {
+    this.service.getPokemonList(this.currentPage * this.currentPageSize, this.currentPageSize).subscribe(resp => {
       this.pokemons = resp.results;
       this.totalCount = resp.count;
-      console.log(this.pokemons);
-      console.log(this.totalCount);
       this.getPokemonsOneByOne();
-      // this.dataSource.paginator = this.paginator;
     });
   }
 
@@ -48,16 +47,58 @@ export class Version1Component implements OnInit {
     this.pokemons.forEach(pokemon => {
       this.service.getPokemonDetails(pokemon).subscribe(data => {
         this.pokemonDetails.push(data);
-        // this.dataSource = [...this.pokemonDetails];
         this.dataSource = new MatTableDataSource(this.pokemonDetails);
+        this.dataSource.paginator = this.paginator;
+        setTimeout(() => this.paginator.length = this.totalCount);
       });
     });
-    console.log(this.pokemonDetails);
+  }
+  //  promjena stranice ili broj itema na stranici poziva se funkcija
+  //  promjeni broj itema na stranici resetiraj array pokemon details
+  public pageEvent(event: PageEvent) {
+    if (!this.type) {
+      if (event.pageSize !== this.currentPageSize) {
+        this.pokemonDetails = [];
+        this.currentPageSize = event.pageSize;
+      }
+      this.service.getPokemonList(event.pageIndex * event.pageSize, event.pageSize).subscribe(resp => {
+        this.pokemons = resp.results;
+        this.getPokemonsOneByOne();
+      });
+    }
+  }
+  public applyFilter(name, type) {
+    this.type = type
+    if(name) {
+      this.service.getPokemonByName(name).subscribe(resp => {
+        this.pokemonDetails = [resp];
+        this.totalCount = 1;
+        this.dataSource = new MatTableDataSource(this.pokemonDetails);
+        this.dataSource.paginator = this.paginator;
+        setTimeout(() => this.paginator.length = this.totalCount);
+      });
+    } else {
+      if (type) {
+        this.service.getPokemonsByType(type).subscribe(resp => {
+          this.pokemons = resp;
+          this.pokemonDetails = [];
+          this.totalCount = resp.length
+          this.getPokemonsOneByOne();
+        });
+      }
+      if (!type && !name) {
+        this.pokemonDetails = [];
+        this.service.getPokemonList(this.currentPage * this.currentPageSize, this.currentPageSize).subscribe(resp => {
+          this.pokemons = resp.results;
+          this.totalCount = resp.count;
+          this.getPokemonsOneByOne();
+        });
+      }
+    }
+  }
+  // input name malim slovom
+  public ConvertToLower(event) {
+    this.name = event.toLowerCase();
   }
 
-  public pageEvent(event: PageEvent)
-  {
-    this.currentPage = event.pageIndex;
-    this.currenPageSize = event.pageSize;
-  }
 }
